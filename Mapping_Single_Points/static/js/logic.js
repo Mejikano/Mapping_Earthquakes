30dccb0f034498c9b22010249791843299a8e478
Mejikano <kano_mejikano@yahoo.com.mx>
2021-09-18 11:05:07 -0400

    adding skill drill

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,14 +7,24 @@ let map = L.map("mapid", {
       40.7, -94.5
     ],
     zoom: 4
-  });
+  }).setView([34.0522, -118.2437], 14);
 
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let marker = L.circleMarker([34.0522, -118.2437], {
+  radius: 300, 
+  color:"black",
+  fillColor:"yellow"
+}).addTo(map);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+     maxZoom: 18,
+    id: "dark-v10",
     accessToken: API_KEY
 });
 
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//var marker = L.marker([51.5, -0.09]).addTo(map);
