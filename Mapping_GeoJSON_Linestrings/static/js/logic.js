f3a8fb2f88eec82c0c20f87424ae6da6ff188755
Mejikano <kano_mejikano@yahoo.com.mx>
2021-09-18 17:47:11 -0400

    Adding GeoJSON lines

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,58 @@
+
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    //id: "mapbox/navigation-night-v1",
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+});
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    //id: "mapbox/navigation-night-v1",
+    id: "mapbox/dark-v10",
+    accessToken: API_KEY
+});
+
+//Create baselayer
+let baseMaps = {
+  'Day Navigation': light,
+  'Night Navigation': dark
+}
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [light]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/Mejikano/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  style:myStyle,
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup(`<h2>Airline:${feature.properties.airline}</h2>Destination: ${feature.properties.dst}`).addTo(map);
+  }
+}).addTo(Map);
+});
+
+
